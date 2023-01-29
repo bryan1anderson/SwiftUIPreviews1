50931f6101d7b534955db013572d6d988cf5e1c2
bryan-anderson <bryan.anderson@domo.com>
2023-01-29 09:47:38 -0700

    Initial Commit

---
+++ SwiftUIPreviews1/SwiftUIPreviews1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPreviews1App.swift
+//  SwiftUIPreviews1
+//
+//  Created by Bryan Anderson on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPreviews1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
